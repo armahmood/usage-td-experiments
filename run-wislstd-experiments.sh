d765080adc48bdb19ed01d8e24b1c59372988693
Ashique Rupam Mahmood <ashique@ualberta.ca>
2015-06-02 21:03:04 -0600

    Created new branch from main repository

--- run-wislstd-experiments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for runseed in {1..5}
-do
-  python pysrc/experiments/stdrwexp.py $runseed StdRWSparseReward results/wislstd-experiments/stdrw-sparse-reward-states/oislstd/ &
-
-  python pysrc/experiments/stdrwexp.py $runseed StdRWSparseReward results/wislstd-experiments/stdrw-sparse-reward-states/wislstd/ &
-
-done
