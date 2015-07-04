02a784cfb80c2eabdf02a9cb827f4dfc26b3a52d
Ashique Rupam Mahmood <ashique@ualberta.ca>
2015-07-04 01:06:49 -0600

    Adding the right configalg files

--- results/rndmdp-experiments/state-10-ftype-binary/td/configalg.py
+++ results/rndmdp-experiments/state-10-ftype-binary/td/configalg.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cPickle as pickle
 
-alphas    = 10**np.arange(-3, 0.1, .25)
+alphas    = 10**np.arange(-3, 1.1, .25)
 lms       = np.concatenate((np.arange(0, .9, .1), np.arange(.9, 1.01, .01)))
 configs     = [
                  {
